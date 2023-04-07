e71b96261b67765931f1e1de0f0c778df460e6d8
huyingg1 <yinggebarry@gmail.com>
2023-04-06 21:20:09 -0400

    image feature

--- public/js/comments.js
+++ public/js/comments.js
@@ -1,26 +1,29 @@
-const newCommentForm = document.getElementById("new-comment-form");
+document.addEventListener("DOMContentLoaded", () => {
+  const newCommentForm = document.getElementById("new-comment-form");
 
-newCommentForm.addEventListener("submit", async (event) => {
-  event.preventDefault();
+  newCommentForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
 
-  const comment_description = document
-    .getElementById("comment_description")
-    .value.trim();
-  const carReview_id = newCommentForm.getAttribute("data-carReviewid");
+    const comment_description = document
+      .getElementById("comment_description")
+      .value.trim();
+    const carReview_id = newCommentForm.getAttribute("data-carReviewid");
 
-  if (!comment_description) {
-    return;
-  }
+    if (!comment_description) {
+      return;
+    }
 
-  const response = await fetch("/api/comments", {
-    method: "POST",
-    body: JSON.stringify({ comment_description, carReview_id }),
-    headers: { "Content-Type": "application/json" },
+    const response = await fetch("/api/comments", {
+      method: "POST",
+      body: JSON.stringify({ comment_description, carReview_id }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.reload();
+    } else {
+      alert("Failed to create comment");
+    }
   });
-
-  if (response.ok) {
-    document.location.reload();
-  } else {
-    alert("Failed to create comment");
-  }
 });
+// js runs only after the DOM has fully loaded
