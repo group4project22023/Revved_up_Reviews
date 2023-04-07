98fdb0385ec29b665232a9acd0353f195368654b
JacksonR-K <jrkbirdy@hotmail.com>
2023-04-07 16:37:07 -0400

    Populated dropdown lists on dashboard

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,4 +1,7 @@
 module.exports = {
+  capitalize: (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  },
   format_time: (date) => {
     if (!date) return "";
     return new Date(date).toLocaleTimeString();
