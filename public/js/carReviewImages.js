2d0617b829172413c66dfc969b804c5c8960d7bf
Terry <misterfreemann@gmail.com>
2023-04-10 12:25:22 -0400

    made app responsive to mobile screen sizes

--- public/js/carReviewImages.js
+++ public/js/carReviewImages.js
@@ -28,23 +28,32 @@ async function loadImages(carMake, carModel) {
 
       const imagesContainer = document.getElementById("images-container");
 
+      const exteriorContainer = document.createElement("div");
+      exteriorContainer.className = "flex flex-col gap-2 items-center";
       const exteriorHeading = document.createElement("h2");
-      exteriorHeading.textContent = "Exterior";
-      imagesContainer.appendChild(exteriorHeading);
+      exteriorHeading.textContent = "Exterior:";
+      exteriorContainer.appendChild(exteriorHeading);
 
       const exteriorImg = document.createElement("img");
       exteriorImg.src = exteriorImage.link;
       exteriorImg.alt = "Exterior image";
-      imagesContainer.appendChild(exteriorImg);
+      exteriorImg.className = "max-[426px]:w-[90%]"
+      exteriorContainer.appendChild(exteriorImg);
+
+      const interiorContainer = document.createElement("div");
+      interiorContainer.className = "flex flex-col gap-2 items-center";
 
       const interiorHeading = document.createElement("h2");
-      interiorHeading.textContent = "Interior";
-      imagesContainer.appendChild(interiorHeading);
+      interiorHeading.textContent = "Interior:";
+      interiorContainer.appendChild(interiorHeading);
 
       const interiorImg = document.createElement("img");
       interiorImg.src = interiorImage.link;
       interiorImg.alt = "Interior image";
-      imagesContainer.appendChild(interiorImg);
+      interiorImg.className = "max-[426px]:w-[90%]"
+      interiorContainer.appendChild(interiorImg);
+      imagesContainer.appendChild(exteriorContainer);
+      imagesContainer.appendChild(interiorContainer);
     } catch (error) {
       console.error("Error fetching images:", error);
     }
